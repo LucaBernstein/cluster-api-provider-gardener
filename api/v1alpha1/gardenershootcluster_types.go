c1ee70627573a2c503579c5487654be317fd1ecc
Luca Bernstein <luca.bernstein@sap.com>
2025-03-13 07:39:24 +0100

    Start filling the Cluster type

--- api/v1alpha1/gardenershootcluster_types.go
+++ api/v1alpha1/gardenershootcluster_types.go
@@ -18,36 +18,34 @@ package v1alpha1
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	gardenercorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
 )
-
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
-// GardenerShootClusterSpec defines the desired state of GardenerShootCluster.
-type GardenerShootClusterSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// Foo is an example field of GardenerShootCluster. Edit gardenershootcluster_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
-}
-
-// GardenerShootClusterStatus defines the observed state of GardenerShootCluster.
-type GardenerShootClusterStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-}
 
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
 
-// GardenerShootCluster is the Schema for the gardenershootclusters API.
+// GardenerShootCluster represents a Shoot cluster.
 type GardenerShootCluster struct {
-	metav1.TypeMeta   `json:",inline"`
+	metav1.TypeMeta `json:",inline"`
+	// Standard object metadata.
+	// +optional
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	Spec   GardenerShootClusterSpec   `json:"spec,omitempty"`
-	Status GardenerShootClusterStatus `json:"status,omitempty"`
+	// Specification of the Shoot cluster.
+	// If the object's deletion timestamp is set, this field is immutable.
+	// +optional
+	Spec   gardenercorev1beta1.ShootSpec `json:"spec,omitempty"`
+	Status GardenerShootClusterStatus    `json:"status,omitempty"`
+}
+
+// GardenerShootClusterStatus defines the observed state of GardenerShootCluster.
+type GardenerShootClusterStatus struct {
+	// Ready denotes that the foo cluster infrastructure is fully provisioned.
+	// NOTE: this field is part of the Cluster API contract and it is used to orchestrate provisioning.
+	// The value of this field is never updated after provisioning is completed. Please use conditions
+	// to check the operational state of the infra cluster.
+	Ready bool `json:"ready"`
 }
 
 // +kubebuilder:object:root=true
@@ -55,10 +53,12 @@ type GardenerShootCluster struct {
 // GardenerShootClusterList contains a list of GardenerShootCluster.
 type GardenerShootClusterList struct {
 	metav1.TypeMeta `json:",inline"`
+	// +optional
 	metav1.ListMeta `json:"metadata,omitempty"`
-	Items           []GardenerShootCluster `json:"items"`
+
+	Items []GardenerShootCluster `json:"items"`
 }
 
 func init() {
-	SchemeBuilder.Register(&GardenerShootCluster{}, &GardenerShootClusterList{})
+	objectTypes = append(objectTypes, &GardenerShootCluster{}, &GardenerShootClusterList{})
 }
