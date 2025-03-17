041e40aa6c0752f614fc1e280265039719a2a29e
Tobias Schlicht <tobias.schlicht@sap.com>
2025-03-17 17:00:37 +0100

    Adapt API
    In the future there will be fields like the controlPlaneEndpoint that needs to be in `.spec`.

--- api/v1alpha1/gardenershootcluster_types.go
+++ api/v1alpha1/gardenershootcluster_types.go
@@ -17,9 +17,8 @@ limitations under the License.
 package v1alpha1
 
 import (
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
 	gardenercorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // +kubebuilder:object:root=true
@@ -38,19 +37,62 @@ type GardenerShootControlPlane struct {
 	// Specification of the Shoot cluster.
 	// If the object's deletion timestamp is set, this field is immutable.
 	// +optional
-	// TODO(tobschli): Change this to Shoot spec and build it in the reconciliation.
-	Spec gardenercorev1beta1.ShootSpec `json:"spec,omitempty"`
+	Spec   GardenerShootControlPlaneSpec   `json:"spec,omitempty"`
+	Status GardenerShootControlPlaneStatus `json:"status,omitempty"`
+}
+
+// GardenerShootControlPlaneSpec represents the Spec of the Shoot Cluster,
+// as well as the fields defined by the Cluster API contract.
+type GardenerShootControlPlaneSpec struct {
 	// +optional
-	Project string                          `json:"project,omitempty"`
-	Status  GardenerShootControlPlaneStatus `json:"status,omitempty"`
+	Project string `json:"project,omitempty"`
+	// + optional
+	ShootSpec gardenercorev1beta1.ShootSpec `json:"shootSpec,omitempty"`
 }
 
 // GardenerShootControlPlaneStatus defines the observed state of GardenerShootControlPlane.
 type GardenerShootControlPlaneStatus struct {
-	// Ready denotes that the foo cluster control plane is fully provisioned.
+	// selector is the label selector in string format to avoid introspection
+	// by clients, and is used to provide the CRD-based integration for the
+	// scale subresource and additional integrations for things like kubectl
+	// describe. The string will be in the same format as the query-param syntax.
+	// More info about label selectors: http://kubernetes.io/docs/user-guide/labels#label-selectors
+	// +optional
+	Selector string `json:"selector,omitempty"`
+
+	// replicas is the total number of machines targeted by this control plane
+	// (their labels match the selector).
+	// +optional
+	Replicas int32 `json:"replicas"`
+
+	// updatedReplicas is the total number of machines targeted by this control plane
+	// that have the desired template spec.
+	// +optional
+	UpdatedReplicas int32 `json:"updatedReplicas"`
+
+	// readyReplicas is the total number of fully running and ready control plane machines.
+	// +optional
+	ReadyReplicas int32 `json:"readyReplicas"`
+
+	// unavailableReplicas is the total number of unavailable machines targeted by this control plane.
+	// This is the total number of machines that are still required for the deployment to have 100% available capacity.
+	// They may either be machines that are running but not yet ready or machines
+	// that still have not been created.
+	// +optional
+	UnavailableReplicas int32 `json:"unavailableReplicas"`
+
+	// initialized denotes that the foo control plane  API Server is initialized and thus
+	// it can accept requests.
 	// NOTE: this field is part of the Cluster API contract and it is used to orchestrate provisioning.
 	// The value of this field is never updated after provisioning is completed. Please use conditions
-	// to check the operational state of the infra cluster.
+	// to check the operational state of the control plane.
+	Initialized bool `json:"initialized"`
+
+	// ready denotes that the foo control plane is ready to serve requests.
+	// NOTE: this field is part of the Cluster API contract and it is used to orchestrate provisioning.
+	// The value of this field is never updated after provisioning is completed. Please use conditions
+	// to check the operational state of the control plane.
+	// +optional
 	Ready bool `json:"ready"`
 }
 
