303c433422db038ac452fe2b6b07c5e2ad4cc6ab
Tobias Schlicht <tobias.schlicht@sap.com>
2025-04-30 14:59:54 +0200

    Enhance printer columns for `GardenerShootCluster`

--- api/infrastructure/v1alpha1/gardenershootcluster_types.go
+++ api/infrastructure/v1alpha1/gardenershootcluster_types.go
@@ -49,6 +49,8 @@ type GardenerShootClusterStatus struct {
 
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
+// +kubebuilder:printcolumn:name="Ready",type=boolean,JSONPath=`.status.ready`
+// +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp"
 
 // GardenerShootCluster is the Schema for the gardenershootclusters API.
 type GardenerShootCluster struct {
